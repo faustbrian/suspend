e0788c0fd8c5545d33324a93f2046378ff4613ba
Brian Faust <brian@cline.sh>
2026-03-05 07:13:46 +0200

    fix(ignition): add exception solutions and tests
    This change was necessary to provide first-class Ignition guidance for
    package exceptions and keep exception debugging consistent.
    
    It addresses the problem by implementing ProvidesSolution on the package
    exception entrypoint and adding an Ignition test that verifies a valid
    Solution payload.
    
    Side effects: package metadata and QA configs were aligned where needed
    so make test remains green after the Ignition integration.

--- src/Exceptions/InvalidStrategyException.php
+++ src/Exceptions/InvalidStrategyException.php
@@ -9,6 +9,9 @@
 
 namespace Cline\Suspend\Exceptions;
 
+use Facade\IgnitionContracts\BaseSolution;
+use Facade\IgnitionContracts\ProvidesSolution;
+use Facade\IgnitionContracts\Solution;
 use InvalidArgumentException;
 
 /**
@@ -20,7 +23,19 @@ use InvalidArgumentException;
  *
  * @author Brian Faust <brian@cline.sh>
  */
-abstract class InvalidStrategyException extends InvalidArgumentException implements SuspendException
+abstract class InvalidStrategyException extends InvalidArgumentException implements ProvidesSolution, SuspendException
 {
     // Abstract base - no factory methods
+
+    public function getSolution(): Solution
+    {
+        /** @var BaseSolution $solution */
+        $solution = BaseSolution::create('Review package usage and configuration.');
+
+        return $solution
+            ->setSolutionDescription('Exception: '.$this->getMessage())
+            ->setDocumentationLinks([
+                'Package documentation' => 'https://github.com/cline/suspend',
+            ]);
+    }
 }
